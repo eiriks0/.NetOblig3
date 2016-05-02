25fb3ca7b26a09fa5bfadf5464c7044099dbf7be
Geelink2 <june.lithell@gmail.com>
2016-05-02 17:48:29 +0200

    NÅ DA

---
+++ Obligatorisk3/Obligatorisk3/Obligatorisk3/Results.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Results.aspx.cs" Inherits="Obligatorisk3.Results" MasterPageFile="~/Site.Master" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server" >
+
+    <div>
+    
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    
+    </div>
+
+    </asp:Content>
---
+++ Obligatorisk3/Obligatorisk3/Obligatorisk3/Results.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web.Security;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+
+namespace Obligatorisk3
+{
+    public partial class Results : System.Web.UI.Page
+    {
+       
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Label1.Text = "Du klarte " + Session["poeng"].ToString() + " av 20 spørsmål";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+            Session.Clear();
+        }
+    }
+}
